26a2b2e374cf0f68f8e240e711a64da1521d898e
Hajin Chung <hajinchung1@gmail.com>
2023-04-27 00:52:54 +0900

    add google analytics w/ partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
+import partytown from "@astrojs/partytown";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()],
-  output: "server",
-  adapter: cloudflare()
+	integrations: [tailwind(), solidJs(), partytown({
+		config: {
+			forward: ["dataLayer.push"],
+		}
+	})],
+	output: "server",
+	adapter: cloudflare()
 });

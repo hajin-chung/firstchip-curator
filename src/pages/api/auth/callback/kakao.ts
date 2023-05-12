e11e29f4589dce8749136136078fcb9fad372534
hajin-chung <hajinchung1@gmail.com>
2023-05-12 11:56:18 +0900

    raw error obj as param on error page

--- src/pages/api/auth/callback/kakao.ts
+++ src/pages/api/auth/callback/kakao.ts
@@ -65,6 +65,6 @@ export const get: APIRoute = async ({ request, redirect }) => {
     });
   } catch (e) {
     console.error(e);
-    return redirect(`/500?message=${JSON.stringify(e)}`, 307);
+    return redirect(`/500?message=${e}`, 307);
   }
 };

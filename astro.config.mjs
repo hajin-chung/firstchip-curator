007d66a2dc4761a050f65b8d1885518511b9627e
hajin-chung <hajinchung1@gmail.com>
2023-05-12 19:08:17 +0900

    define env vars in config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,18 @@ import partytown from "@astrojs/partytown";
 
 import solidJs from "@astrojs/solid-js";
 
+const envNames = [
+	"DATABASE_HOST",
+	"DATABASE_USERNAME",
+	"DATABASE_PASSWORD",
+	"PUBLIC_KAKAO_REST_API_KEY",
+	"PUBLIC_KAKAO_REDIRECT_URL",
+	"R2_ACCESS_KEY",
+	"R2_SECRET_KEY",
+	"BUCKET_NAME",
+	"CLOUDFLARE_ACCOUNT_ID",
+];
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), solidJs(), partytown({
@@ -13,5 +25,11 @@ export default defineConfig({
 		}
 	})],
 	output: "server",
-	adapter: cloudflare()
+	adapter: cloudflare(),
+	vite: {
+		define: envNames.reduce((pv, v) => {
+			pv[`process.env.${v}`] = JSON.stringify(process.env[v])
+			return pv;
+		}, {}),
+	}
 });

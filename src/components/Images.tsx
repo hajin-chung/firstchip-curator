6403f98e5b12690d9ac521a9a0b9c6915ea2b363
hajin-chung <hajinchung1@gmail.com>
2023-05-23 14:44:13 +0900

    image skeletn & carousel

---
+++ src/components/Images.tsx
@@ -0,0 +1,73 @@
+import { createSignal, type Component, onMount } from "solid-js";
+
+type Props = {
+  imageUrls: string[];
+};
+
+export const Images: Component<Props> = ({ imageUrls }) => {
+  const [loadCount, setLoadCount] = createSignal(0);
+  const [imageIndex, setImageIndex] = createSignal(0);
+  const [mousePos, setMousePos] = createSignal<
+    { x: number; y: number } | undefined
+  >();
+
+  return (
+    <div class="w-full h-[540px] rounded-lg flex flex-col items-center gap-4">
+      <div
+        class="w-full h-full relative overflow-hidden"
+        classList={{
+          loading: loadCount() !== imageUrls.length,
+        }}
+        onMouseDown={(evt) => setMousePos({ x: evt.clientX, y: evt.clientY })}
+        onMouseUp={(evt) => {
+          const pos = mousePos();
+          if (!pos) return;
+          const dx = pos.x - evt.clientX;
+          const dy = pos.y - evt.clientY;
+
+          if (dx > 0) {
+            setImageIndex((idx) => {
+              if (idx + 1 < imageUrls.length) return idx + 1;
+              else return idx;
+            });
+          } else if (dx < 0) {
+            setImageIndex((idx) => {
+              if (idx - 1 >= 0) return idx - 1;
+              else return idx;
+            });
+          }
+        }}
+      >
+        {imageUrls.map((image, idx) => (
+          <div
+            class="absolute top-0 left-0 h-full w-full flex justify-center transition"
+            classList={{
+              "opacity-100": imageIndex() === idx,
+              "opacity-0": imageIndex() !== idx,
+            }}
+          >
+            <img
+              src={image}
+              class="drop-shadow-2xl rounded-lg self-center max-h-full  max-w-full transition opacity-0"
+              onLoad={(evt) => {
+                evt.currentTarget.classList.remove("opacity-0");
+                setLoadCount((cnt) => cnt + 1);
+              }}
+              onDragStart={(evt) => evt.preventDefault()}
+              draggable={false}
+            />
+          </div>
+        ))}
+      </div>
+      <div class="flex gap-2">
+        {imageUrls.map((_, idx) => (
+          <button
+            class="rounded-full w-2 h-2 bg-gray-300 transition"
+            classList={{ "bg-gray-600": imageIndex() === idx }}
+            onClick={() => setImageIndex(idx)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};

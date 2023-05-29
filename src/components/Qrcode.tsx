10ba62063a40ac87da7c65bc9f3b78f942e6252a
hajin-chung <hajinchung1@gmail.com>
2023-05-29 21:12:33 +0900

    art cards print page

--- src/components/Qrcode.tsx
+++ src/components/Qrcode.tsx
@@ -1,30 +1,34 @@
 import { Component, createSignal, onMount } from "solid-js";
 import * as qr from "qrcode";
 
-export const Qrcode: Component = () => {
-  const [show, setShow] = createSignal(false);
+type Props =
+  | {
+      url: string;
+      useCurrent: false;
+    }
+  | { url: ""; useCurrent: true };
+
+export const Qrcode: Component<Props> = ({ url, useCurrent }) => {
   const [code, setCode] = createSignal<string>();
 
-  onMount(async () => {
-    if (!window) return;
-    if (window.location.href) {
+  onMount(() => {
+    if (useCurrent) {
+			console.log(window.location.href);
       qr.toDataURL(window.location.href).then((s) => {
         setCode(s);
-        console.log(s);
+      });
+    } else if (!useCurrent) {
+      qr.toDataURL(url).then((s) => {
+        setCode(s);
       });
     }
   });
 
   return (
     <div class="flex flex-col gap-2 self-end">
-      {show() && (
-        <div class="w-20 h-20">
-          <img src={code()} />
-        </div>
-      )}
-      <button class="hover-bg p-1" onClick={() => setShow((s) => !s)}>
-        QR코드 보기
-      </button>
+      <div class="w-20 h-20">
+        <img src={code()} />
+      </div>
     </div>
   );
 };

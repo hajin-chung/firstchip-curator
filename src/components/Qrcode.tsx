a9f482bc7535148a6324426644e30c5876b1d6dc
hajin-chung <hajinchung1@gmail.com>
2023-05-29 15:41:04 +0900

    generate qrcode

---
+++ src/components/Qrcode.tsx
@@ -0,0 +1,30 @@
+import { Component, createSignal, onMount } from "solid-js";
+import * as qr from "qrcode";
+
+export const Qrcode: Component = () => {
+  const [show, setShow] = createSignal(false);
+  const [code, setCode] = createSignal<string>();
+
+  onMount(async () => {
+    if (!window) return;
+    if (window.location.href) {
+      qr.toDataURL(window.location.href).then((s) => {
+        setCode(s);
+        console.log(s);
+      });
+    }
+  });
+
+  return (
+    <div class="flex flex-col gap-2 self-end">
+      <button class="hover-bg p-1" onClick={() => setShow((s) => !s)}>
+        QR코드 보기
+      </button>
+      {show() && (
+        <div class="w-20 h-20">
+          <img src={code()} />
+        </div>
+      )}
+    </div>
+  );
+};

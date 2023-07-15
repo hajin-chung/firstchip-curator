e83a364da78d6aafc00a696c531eab6aa0edcfaf
hajin-chung <hajinchung1@gmail.com>
2023-07-15 17:05:27 +0900

    some changes

--- src/components/ArtList.tsx
+++ src/components/ArtList.tsx
@@ -1,6 +1,5 @@
 import { createSignal, type Component, onMount } from "solid-js";
 import { RouterOutput, client } from "@lib/client";
-import { ArtStatusBadge } from "./ArtStatusBadge";
 
 type Props = {
   artistId: string;
@@ -19,20 +18,22 @@ export const ArtList: Component<Props> = ({ artistId }) => {
   });
 
   return (
-    <>
+    <div class="w-full relative">
       <p class="text-lg font-bold mt-10">작품 목록</p>
       <div class="flex flex-col gap-40 items-center mt-20">
-        {arts().map((art) => (
-          <>
-            <ArtCard {...art} artistId={artistId} />
-            <div class="w-[20vw] bg-gray-500 h-[2px]" />
-          </>
-        ))}
+        {isLoading() && <LoadingArtCard />}
+        {!isLoading() &&
+          arts().map((art) => (
+            <>
+              <ArtCard {...art} artistId={artistId} />
+              <div class="w-[20vw] bg-gray-500 h-[2px]" />
+            </>
+          ))}
       </div>
       {arts().length === 0 && (
         <div class="self-center text-gray-500 mt-10">작품 없음</div>
       )}
-    </>
+    </div>
   );
 };
 
@@ -43,7 +44,7 @@ const ArtCard: Component<
     <div class="flex gap-10 flex-col justify-center">
       <img
         src={`/image?id=${thumbnail}`}
-        class="max-h-[80vh] object-contain group-hover:blur-sm transition"
+        class="max-h-[80vh] object-contain group-hover:blur-sm transition drop-shadow-2xl"
       />
       <div class="flex flex-col items-center">
         <a href={`/art/${artistId}/${id}`} class="font-extrabold text-xl">
@@ -52,7 +53,19 @@ const ArtCard: Component<
         <p />
         {price ? <p>{price} &#8361; </p> : <p>가격 미정</p>}
         <p class="text-sm whitespace-pre">{description}</p>
-        {/* <ArtStatusBadge status={status} /> */}
+      </div>
+    </div>
+  );
+};
+
+const LoadingArtCard: Component = () => {
+  return (
+    <div class="flex gap-10 flex-col justify-center items-center h-[50vh] w-[40vw]">
+      <div class="skeleton rounded-lg" />
+      <div class="flex flex-col items-center w-[10vw] h-[5vh] rounded-lg overflow-clip">
+        <div class="skeleton" />
+        <p />
+        <p class="text-sm whitespace-pre skeleton" />
       </div>
     </div>
   );
